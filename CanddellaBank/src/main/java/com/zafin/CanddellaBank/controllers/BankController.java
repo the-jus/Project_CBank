c8526390a37cfdf5f93a652a72b0bfd5e7535eb5
Abhijithmanoj <abhijithmanoj72@gmail.com>
2023-04-17 11:29:27 +0530

    rate db entry done

--- CanddellaBank/src/main/java/com/zafin/CanddellaBank/controllers/BankController.java
+++ CanddellaBank/src/main/java/com/zafin/CanddellaBank/controllers/BankController.java
@@ -11,4 +11,8 @@ public class BankController {
     public String showLoginPage(){
         return "login/loginpage";
     }
+    @RequestMapping("/showrateconfig")
+    public String showRateConfig(){
+        return "configure_rate";
+    }
 }
